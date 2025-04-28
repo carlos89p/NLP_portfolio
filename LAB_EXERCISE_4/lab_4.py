7218b1c9addd5350903f5d5a52d00b60c8c7d3e1
Carlos Illan <carlos.illan.01@uie.edu>
2025-04-28 11:29:48 +0200

    speech to text

--- LAB_EXERCISE_4/lab_4.py
+++ LAB_EXERCISE_4/lab_4.py
@@ -14,3 +14,23 @@ def local_text_to_speech(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+
+# ----------------------------
+# LOCAL Speech-To-Text (STT)
+# ----------------------------
+def local_speech_to_text():
+    print("[Local STT] Listening...")
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio)
+        print(f"[Local STT] Recognized: {text}")
+        return text
+    except sr.UnknownValueError:
+        print("[Local STT] Could not understand audio.")
+        return ""
+    except sr.RequestError as e:
+        print(f"[Local STT] Error with service; {e}")
+        return ""
